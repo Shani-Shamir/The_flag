6b1d2e153bec804ff4f0ed01a55ae2d6e418f980
mayanevo <mayanevo8910@gmail.com>
2025-08-24 12:09:12 +0300

    COMMIT

--- screen.py
+++ screen.py
@@ -17,6 +17,18 @@ def draw_bush(x,y):
     bush = pygame.transform.scale(bush, bush_size)
     screen.blit(bush,(x,y))
 
+def create_lists():
+    list_x = []
+    list_y = []
+    for i in range(0, 1001, 20):
+        list_x.append(i)
+    for i in range(0, 501, 20):
+        list_y.append(i)
+    return list_x,list_y
+
+def draw_random_bushes():
+    x,y = create_lists()
+    random_place = (random.choice(x),random.choice(y))
 
 while True:
     for event in pygame.event.get():
