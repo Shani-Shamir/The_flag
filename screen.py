928fd411faa971c668d409c8efa00a21ce319fa3
mayanevo <mayanevo8910@gmail.com>
2025-08-24 11:41:23 +0300

    COMMIT

---
+++ screen.py
@@ -0,0 +1,29 @@
+#יצירת מסך
+import pygame
+from sys import exit
+
+import consts
+
+pygame.init()
+screen = pygame.display.set_mode((1000,800))
+pygame.display.set_caption(consts.TITLE)
+
+def background():
+    screen.fill(consts.GREEN)
+
+def draw_bush(x,y):
+    bush = pygame.image.load(consts.GRASS_IMG)
+    screen.blit(bush,(x,y))
+
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    background()
+    draw_bush(60,60)
+    pygame.display.flip()
+
+
