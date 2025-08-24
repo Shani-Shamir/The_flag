8358f269859feca4d908b1ed1c320d7c41084549
Project <emmashir230307@gmail.com>
2025-08-24 15:38:07 +0300

    g

--- game_field.py
+++ game_field.py
@@ -1,6 +1,16 @@
 import consts
+import random
 matrix = []
 
 for i in range(consts.ROW):
     for j in range(consts.COL) :
-        matrix.append("EMPTY")
+        matrix.append(consts.NO_MINE)
+
+def random_place_mine():
+
+    while True:
+        random_matrix_place = matrix[random.randint(0, consts.ROW)][0, consts.COL]
+        if random_matrix_place == consts.NO_MINE:
+            random_matrix_place = consts.MINE
+
+
