3487a92f86e66361dce238fb6a63a5918f9d63cf
Project <emmashir230307@gmail.com>
2025-08-24 11:35:47 +0300

    h

---
+++ consts.py
@@ -0,0 +1,23 @@
+START_MASSAGE = "Welcome to The Flag game. Have Fun!"
+START_LOCATION =
+NO_MINE = "ENPTY"
+
+GREEN = (159,226,191)
+
+MOVE_LEFT = "left"
+MOVE_RIGHT = "right"
+MOVE_UP = "up"
+MOVE_DOWN = "down"
+
+
+EXPLOTION_IMG = "explotion.png"
+FLAG_IMG = "flag.png"
+GRASS_IMG = "grass.png"
+INJURY_IMG = "injury.png"
+MINE_IMG = "mine.png"
+SNAKE_IMG = "snake.png"
+SOLIDER_IMG = "soldier.png"
+SOLIDER_2_IMG = "solider_2.png"
+SOLIDER_NIGTH = "solider_nigth.png"
+
+
