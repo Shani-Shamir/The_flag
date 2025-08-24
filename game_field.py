e4888c089daeb08224a0643f6911538888e28a62
Project <emmashir230307@gmail.com>
2025-08-24 16:50:45 +0300

    g

--- game_field.py
+++ game_field.py
@@ -1,16 +1,63 @@
 import consts
 import random
+import pygame
+import Screen
 matrix = []
 
 for i in range(consts.ROW):
     for j in range(consts.COL) :
         matrix.append(consts.NO_MINE)
 
+# def random_place_mine():
+#     count = 0
+#     while count <= 20 :
+#         random_row = random.randint(0,consts.ROW)
+#         random_col = random.randint(0, consts.COL)
+#         if matrix[random_row][random_col] == consts.NO_MINE:
+#             matrix[random_row][random_col] = consts.MINE
+#             count += 1
+# print(matrix)
+
+
+def create_mine_lists():
+    list_row = []
+    list_col = []
+    for i in range(0,consts.ROW):
+        list_row.append(i)
+    for i in range(0, consts.COL):
+        list_col.append(i)
+    return list_row,list_col
+
 def random_place_mine():
+    row,col = create_mine_lists()
+    random_x = random.choice(row)
+    random_y = random.choice(col)
+    row.remove(random_x)
+    col.remove(random_y)
+    return random_x,random_y
 
-    while True:
-        random_matrix_place = matrix[random.randint(0, consts.ROW)][0, consts.COL]
-        if random_matrix_place == consts.NO_MINE:
-            random_matrix_place = consts.MINE
+def add_random_mine_matrix():
+    for i in range(20):
+        random_row,random_col = random_place_mine()
+        matrix[random_row][random_col] = consts.MINE
+
+create_mine_lists()
+random_place_mine()
+add_random_mine_matrix()
+print(matrix)
 
 
+
+# def player_move(screen):
+#     x, y = 0, 0
+#     keys = pygame.key.get_pressed()
+#     if keys[pygame.K_RIGHT]:
+#         x += consts.SOLIDER_STEP
+#     if keys[pygame.K_LEFT]:
+#         x -= consts.SOLIDER_STEP
+#     if keys[pygame.K_UP]:
+#         y -= consts.SOLIDER_STEP
+#     if keys[pygame.K_DOWN]:
+#         y += consts.SOLIDER_STEP
+#     Screen.draw_soldier(screen,x,y)
+# pygame.display.flip()
--- main2.py
+++ main2.py
@@ -17,20 +17,8 @@ Screen.draw_flag(screen)
 #             exit()
 #     pygame.display.flip()
 
-x,y =0,0
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_RIGHT]:
-            x += consts.SOLIDER_STEP
-        if keys[pygame.K_LEFT]:
-            x -= consts.SOLIDER_STEP
-        if keys[pygame.K_UP]:
-            y -= consts.SOLIDER_STEP
-        if keys[pygame.K_DOWN]:
-            y += consts.SOLIDER_STEP
-        Screen.draw_soldier(screen,x,y)
-    pygame.display.flip()
