06c6a82c0123e1ec272027ac5dbb324fdadcc956
Project <emmashir230307@gmail.com>
2025-08-25 11:22:28 +0300

    g

--- main2.py
+++ main2.py
@@ -1,7 +1,7 @@
 import pygame
 import consts
 import Screen
-from game_field import touched_flag
+import game_field
 
 # שינוי
 screen = Screen.create_screen()
@@ -29,47 +29,53 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-
         # x, y = 0, 0
         # keys = pygame.key.get_pressed()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
+            screen2.blit(screen2, (0, 0))
+            if event.key == pygame.K_RIGHT and 0 <= soldier.x <= consts.WINDOW_WIDTH:
                 soldier.x += consts.SOLIDER_STEP
-            if event.key == pygame.K_LEFT:
+            if event.key == pygame.K_LEFT and 0 <= soldier.x <= consts.WINDOW_WIDTH:
                 soldier.x -= consts.SOLIDER_STEP
-            if event.key == pygame.K_UP:
+            if event.key == pygame.K_UP and 0 <= soldier.y <= consts.WINDOW_HEIGHT:
                 soldier.y -= consts.SOLIDER_STEP
-            if event.key == pygame.K_DOWN:
+            if event.key == pygame.K_DOWN and 0 <= soldier.y <= consts.WINDOW_HEIGHT:
                 soldier.y += consts.SOLIDER_STEP
         Screen.draw_soldier(screen2, soldier.x, soldier.y)
         screen2.blit(screen2, (0, 0))
-
         pygame.display.update()
         #
         # if event.type == pygame.KEYUP:
         #     screen2.blit(screen2, (0, 0))
 
             # pygame.display(screen2)
-
-    #
     pygame.display.flip()
 
-running = True
-x, y = 0, 0
-while running :
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_RIGHT] and 0 <= x <= consts.WINDOW_WIDTH:
-        x += consts.SOLIDER_STEP
-    if keys[pygame.K_LEFT] and 0 <= x <= consts.WINDOW_WIDTH:
-        x -= consts.SOLIDER_STEP
-    if keys[pygame.K_UP] and 0 <= y <= consts.WINDOW_HEIGHT:
-        y -= consts.SOLIDER_STEP
-    if keys[pygame.K_DOWN] and 0 <= y <= consts.WINDOW_HEIGHT:
-        y += consts.SOLIDER_STEP
-    if keys == pygame.QUIT:
-        pygame.quit()
-        exit()
-    if touched_flag(x,y):
-        running = False
+# running = True
+# while running:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             pygame.quit()
+#             exit()
 #
+#         # x, y = 0, 0
+#         # keys = pygame.key.get_pressed()
+#         if event.type == pygame.KEYDOWN:
+#             if event.key == pygame.K_RIGHT and 0 <= soldier.x <= consts.WINDOW_WIDTH:
+#                 soldier.x += consts.SOLIDER_STEP
+#             if event.key == pygame.K_LEFT and 0 <= soldier.x <= consts.WINDOW_WIDTH:
+#                 soldier.x -= consts.SOLIDER_STEP
+#             if event.key == pygame.K_UP and 0 <= soldier.y <= consts.WINDOW_HEIGHT:
+#                 soldier.y -= consts.SOLIDER_STEP
+#             if event.key == pygame.K_DOWN and 0 <= soldier.y <= consts.WINDOW_HEIGHT:
+#                 soldier.y += consts.SOLIDER_STEP
+#             if game_field.touched_flag(soldier.x, soldier.y):
+#                 pygame.quit()
+#                 running = False
+#         # Screen.draw_soldier(screen2, soldier.x, soldier.y)
+#         screen2.blit(screen2, (0, 0))
+#         pygame.display.update()
+#
+#             # pygame.display(screen2)
+#     pygame.display.flip()
 
