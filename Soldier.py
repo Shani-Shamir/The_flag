081747670eceb65c0133841912e12fda5ddcc901
mayanevo <mayanevo8910@gmail.com>
2025-08-25 12:06:01 +0300

    COMMIT

--- Soldier.py
+++ Soldier.py
@@ -4,6 +4,7 @@ import Screen
 import game_field
 from consts import SOLDIER
 from game_field import matrix_soldier
+from game_field import matrix
 
 soldier_body = {"head" : None,
                 "neck" : None,
@@ -20,35 +21,50 @@ soldier_body["legs"] = [(0,60) , (20,60)]
 # starting position matrix
 for i in range(4):
     for j in range(2):
-        matrix_soldier[i][j] = consts.SOLDIER
+        if i == 3:
+            matrix_soldier[i][j] = consts.FEET
+        else:
+            matrix_soldier[i][j] = consts.BODY
 
 def right():
     for i in range(consts.ROW):
         for j in range(consts.COL):
-            if matrix_soldier[i][j] == consts.SOLDIER:
-                matrix_soldier[i][j] = consts.EMPTY
-                matrix_soldier[i][j+1] = consts.SOLDIER
+            if matrix_soldier[i][j] == consts.FEET:
+                matrix_soldier[i][j] = consts.NO_MINE
+                matrix_soldier[i][j+1] = consts.FEET
+            if matrix_soldier[i][j] == consts.BODY:
+                matrix_soldier[i][j] = consts.NO_MINE
+                matrix_soldier[i][j + 1] = consts.BODY
+
 def left():
     for i in range(consts.ROW):
         for j in range(consts.COL):
-            if matrix_soldier[i][j] == consts.SOLDIER:
-                matrix_soldier[i][j] = consts.EMPTY
-                matrix_soldier[i][j-1] = consts.SOLDIER
+            if matrix_soldier[i][j] == consts.FEET:
+                matrix_soldier[i][j] = consts.NO_MINE
+                matrix_soldier[i][j-1] = consts.FEET
+            if matrix_soldier[i][j] == consts.BODY:
+                matrix_soldier[i][j] = consts.NO_MINE
+                matrix_soldier[i][j - 1] = consts.BODY
 
 def up():
     for i in range(consts.ROW):
         for j in range(consts.COL):
-            if matrix_soldier[i][j] == consts.SOLDIER:
-                matrix_soldier[i][j] = consts.EMPTY
-                matrix_soldier[i+1][j] = consts.SOLDIER
+            if matrix_soldier[i][j] == consts.FEET:
+                matrix_soldier[i][j] = consts.NO_MINE
+                matrix_soldier[i+1][j] = consts.FEET
+            if matrix_soldier[i][j] == consts.BODY:
+                matrix_soldier[i][j] = consts.NO_MINE
+                matrix_soldier[i+1][j] = consts.BODY
 
 def down():
     for i in range(consts.ROW):
         for j in range(consts.COL):
-            if matrix_soldier[i][j] == consts.SOLDIER:
-                matrix_soldier[i][j] = consts.EMPTY
-                matrix_soldier[i-1][j] = consts.SOLDIER
-
+            if matrix_soldier[i][j] == consts.FEET:
+                matrix_soldier[i][j] = consts.NO_MINE
+                matrix_soldier[i-1][j] = consts.FEET
+            if matrix_soldier[i][j] == consts.BODY:
+                matrix_soldier[i][j] = consts.NO_MINE
+                matrix_soldier[i-1][j] = consts.BODY
 
 def update_soldier_place():
     for event in pygame.event.get():
@@ -61,5 +77,11 @@ def update_soldier_place():
                 up()
             if event.key == pygame.K_DOWN:
                 down()
+
+def soldier_touch_mine():
+    for row in range (consts.ROW):
+        for col in range(consts.COL):
+            if matrix[row][col]
+
 
 update_soldier_place()
